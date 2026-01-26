76118cc9ff057f3fe6298d9ad37795ead8cb7637
oforotitodirichukwu-afk <oforotitodirichukwu@gmail.com>
2026-01-26 23:25:24 +0100

    chore: remove duplicate terraform resources and fix bucket name

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-# This creates the "box" in AWS to store your backend Docker image
 resource "aws_ecr_repository" "backend" {
   name                 = "starttech-backend"
   image_tag_mutability = "MUTABLE"
@@ -7,9 +6,18 @@ resource "aws_ecr_repository" "backend" {
     scan_on_push = true
   }
 }
-resource "aws_s3_bucket" "app_storage" {
-  bucket = "starttech-storage-ofor-1535" # Ensure this name is unique!
+
+
+resource "aws_ecr_repository" "frontend" {
+  name                 = "starttech-frontend"
+  image_tag_mutability = "MUTABLE"
 }
+
+
+resource "aws_s3_bucket" "app_storage" {
+  bucket = "starttech-ofor-1535-unique-storage" # Keep the unique name
+}
+
 
 resource "aws_s3_bucket_public_access_block" "app_storage_block" {
   bucket = aws_s3_bucket.app_storage.id

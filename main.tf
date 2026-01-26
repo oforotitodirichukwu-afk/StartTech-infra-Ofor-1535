445a9487ac80723af5662fab23d1d219d6a13859
oforotitodirichukwu-afk <oforotitodirichukwu@gmail.com>
2026-01-26 21:45:20 +0100

    feat: add S3 bucket for application storage

--- main.tf
+++ main.tf
@@ -7,3 +7,15 @@ resource "aws_ecr_repository" "backend" {
     scan_on_push = true
   }
 }
+resource "aws_s3_bucket" "app_storage" {
+  bucket = "starttech-storage-ofor-1535" # Ensure this name is unique!
+}
+
+resource "aws_s3_bucket_public_access_block" "app_storage_block" {
+  bucket = aws_s3_bucket.app_storage.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}

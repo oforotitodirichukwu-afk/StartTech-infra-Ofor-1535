91a66d08a6eb47808bcb742d33ecc77e12d1e272
oforotitodirichukwu-afk <oforotitodirichukwu@gmail.com>
2026-01-28 22:01:19 +0100

    Fix: Resolved duplicate output in networking module

--- terraform/modules/networking/outputs.tf
+++ terraform/modules/networking/outputs.tf
@@ -5,7 +5,3 @@ output "vpc_id" {
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
-output "backend_sg_id" {
-  description = "The ID of the security group for the backend"
-  value       = aws_security_group.alb_sg.id 
-}
